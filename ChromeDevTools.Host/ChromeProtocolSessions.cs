9880ff5db28989a7312597c998cd4b646c43e614
Frederic Forjan <fforjan@free.fr>
2019-10-26 13:22:52 -0700

    hardening the connection/disconnection

---
+++ ChromeDevTools.Host/ChromeProtocolSessions.cs
@@ -0,0 +1,49 @@
+﻿namespace ChromeDevTools.Host
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class ChromeProtocolSessions
+    {
+        private readonly object locker = new object();
+
+        private List<ChromeProtocolSession> sessions = new List<ChromeProtocolSession>();
+
+        public IDisposable Add(ChromeProtocolSession session)
+        {
+            lock (locker)
+            {
+                sessions = new List<ChromeProtocolSession>(sessions) { session };
+            }
+
+            return new Dispose(this, session);
+        }
+
+        public Task ForEach(Func<ChromeProtocolSession, Task> chromeSessionAction)
+        {
+            return Task.WhenAll(sessions.Select(chromeSessionAction));
+        }
+
+        private class Dispose : IDisposable
+        {
+            private readonly ChromeProtocolSessions sessions;
+            private readonly ChromeProtocolSession session;
+
+            public Dispose(ChromeProtocolSessions sessions, ChromeProtocolSession session)
+            {
+                this.sessions = sessions;
+                this.session = session;
+            }
+
+            void IDisposable.Dispose()
+            {
+                lock (sessions.locker)
+                {
+                    sessions.sessions = sessions.sessions.Where(_ => _ != session).ToList();
+                }
+            }
+        }
+    }
+}
